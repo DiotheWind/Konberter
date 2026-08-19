c8f2bb3485106c197d6b70709a91083d4920af48
Deo <diothewind@gmail.com>
2026-08-19 17:36:56 +0800

    Create frontend design for the index file using Tailwind

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"))
+app.use("/icons", express.static("node_modules/bootstrap-icons/font"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
