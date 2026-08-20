35e87f627f343724c211e5cd93638b87038b0533
Deo <diothewind@gmail.com>
2026-08-20 10:37:31 +0800

    Add functionality to select all supported currencies from the API

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 const apiKey = process.env.API_KEY;
-const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}`
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}`;
+const supportedCurrencies = (async () => {
+    const response = (await axios.get(`${apiURL}/codes`)).data.supported_codes;
+    let currencies = [];
 
-app.use(express.static("public"))
+    for (const code of response) {
+        currencies.push(code[0]);
+    }
+
+    return currencies;
+})();
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    res.render("index.ejs", { supportedCurrencies: await supportedCurrencies });
 });
 
 app.post("/", async (req, res) => {
@@ -22,6 +32,7 @@ app.post("/", async (req, res) => {
     const convertedMoney = (await axios.get(`${apiURL}/pair/${baseCurrency}/${targetCurrency}/${money}`)).data.conversion_result;
 
     const context = {
+        supportedCurrencies: await supportedCurrencies,
         baseMoney: money,
         convertedMoney: convertedMoney.toFixed(2),
         baseCurrency: baseCurrency,
